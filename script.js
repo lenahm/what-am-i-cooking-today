7d7c7a9b34057e3c315e24b7b013206987c6a180
Lena Herrmann <herrmann-lena@t-online.de>
2022-05-10 10:53:13 +0200

    initial project setup, markup with dummy code and basic styling

---
+++ script.js
@@ -0,0 +1,6 @@
+// - meal by name           			www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+// - random meal        			    www.themealdb.com/api/json/v1/1/random.php
+// - meals by categories     			www.themealdb.com/api/json/v1/1/categories.php liefert Kategorien
+// 									    www.themealdb.com/api/json/v1/1/filter.php?c=Seafood filtert nach Kategorien 
+// - meals by nationalities     		www.themealdb.com/api/json/v1/1/list.php?a=list liefert Nationalitäten
+// 									    www.themealdb.com/api/json/v1/1/filter.php?a=Canadian filtert nach Nationalitäten 
